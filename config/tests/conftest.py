3673db4e7a64dbafb809865021194979cab3986e
Emcent <mic_bezzy@yahoo.com>
2024-11-22 14:03:56 +0100

    Updated the Pytest

--- config/tests/conftest.py
+++ config/tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from pytest_factoryboy import register
 from rest_framework.test import APIClient
-from .factories import CategoryFactory, BrandFactory, ProductFactory
+from .factories import *
 
 register(CategoryFactory)
 register(BrandFactory)
 register(ProductFactory)
+register(ProductLineFactory)
 
 
 @pytest.fixture
